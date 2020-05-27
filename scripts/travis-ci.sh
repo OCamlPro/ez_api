5761e1c10e6d9600f79bb5e2ea9bf16c32706312
Alain Mebsout <alain.mebsout@origin-labs.com>
2020-05-27 20:31:55 +0200

    Fix travis

---
+++ scripts/travis-ci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export OPAMYES=1 OPAMVERBOSE=1
+eval `opam config env`
+
+echo Architecture
+uname -a
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+PREFIX=$HOME/.opam/$OCAML_VERSION
+
+opam pin add .
+opam install .
+opam remove .
