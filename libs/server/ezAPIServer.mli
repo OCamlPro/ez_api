79a56500498bbea00279cc8bfa0a1dfc6770c8b6
Maxime Levillain <maxime.levillain@origin-labs.com>
2020-09-30 10:36:58 +0200

    add optional catch function to catch exception

--- libs/server/ezAPIServer.mli
+++ libs/server/ezAPIServer.mli
@@ -1,1 +1,2 @@
-val server : ?require_method:bool -> (int * EzAPIServerUtils.server_kind) list -> unit Lwt.t
+val server : ?require_method:bool -> ?catch:(string -> exn -> (int * EzAPIServerUtils.reply) Lwt.t) ->
+  (int * EzAPIServerUtils.server_kind) list -> unit Lwt.t
