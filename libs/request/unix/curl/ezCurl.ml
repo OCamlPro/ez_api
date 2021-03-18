79f123c4fe81d457e0fe2b47ffa12a485f19facf
Maxime Levillain <maxime.levillain@origin-labs.com>
2021-03-18 13:21:41 +0100

    fix curl method request

--- libs/request/unix/curl/ezCurl.ml
+++ libs/request/unix/curl/ezCurl.ml
@@ -1,66 +1,29 @@
-let verbose = match Sys.getenv_opt "EZAPICLIENT" with
-  | Some "true" | Some "1" -> true
-  | _ -> false
+let make ?meth ?headers ?content ?content_type url msg f =
+  let msg = if msg = "" then None else (Some msg) in
+  EzCurl_common.log ?meth url msg;
+  let rc, data =
+    try
+      let r, c = EzCurl_common.init ?meth ?headers ?content ?content_type url in
+      Curl.perform c;
+      let rc = Curl.get_responsecode c in
+      Curl.cleanup c;
+      rc, Buffer.contents r
+    with _ -> -1, ""
+  in
+  EzCurl_common.log ~meth:("RECV " ^ string_of_int rc) url msg;
+  if rc >= 200 && rc < 300 then
+    try f (Ok data) with _ -> ()
+  else
+    try f (Error (rc, Some data)) with _ -> ()
 
 module Interface = struct
 
-  let log ?(meth="GET") url = function
-    | "" -> ()
-    | msg -> Printf.printf "[>%s %s %s ]\n%!" msg meth url
+  let get ?(meth="GET") msg url ?headers f =
+    make ~meth ?headers url msg f
 
-  let writer_callback a d =
-    Buffer.add_string a d;
-    String.length d
-
-  let initialize_connection url =
-    let r = Buffer.create 16384
-    and c = Curl.init () in
-    Curl.set_timeout c 30;      (* Timeout *)
-    Curl.set_sslverifypeer c false;
-    Curl.set_sslverifyhost c Curl.SSLVERIFYHOST_EXISTENCE;
-    Curl.set_writefunction c (writer_callback r);
-    Curl.set_tcpnodelay c true;
-    Curl.set_verbose c verbose;
-    Curl.set_post c false;
-    Curl.set_url c url;
-    r,c
-
-  let make prepare url ~headers msg f =
-    let rc, data =
-      try
-        let r, c = initialize_connection url in
-        prepare c;
-        Curl.set_httpheader c (
-          List.map (fun (name, value) ->
-              Printf.sprintf "%s: %s" name value
-            ) headers);
-        Curl.perform c;
-        let rc = Curl.get_responsecode c in
-        Curl.cleanup c;
-        rc, Buffer.contents r
-      with _ -> -1, ""
-    in
-    log ~meth:("RECV " ^ string_of_int rc) url msg;
-    if rc >= 200 && rc < 300 then
-      try f (Ok data) with _ -> ()
-    else
-      try f (Error (rc, Some data)) with _ -> ()
-
-  let get ?(meth="GET") msg url ?(headers=[]) f =
-    log ~meth url msg;
-    make ~headers (fun c ->
-        Curl.set_post c false) url msg f
-
-  let post ?(meth="POST") ?(content_type = "application/json") ?(content="{}")
-      msg url ?(headers=[]) f =
-    log ~meth url msg;
-    let headers = ("Content-Type", content_type) :: headers in
-    make ~headers (fun c ->
-        if meth = "PUT" then Curl.set_put c true else Curl.set_post c true;
-        Curl.set_postfields c content;
-        Curl.set_postfieldsize c (String.length content);
-      ) url msg f
-
+  let post ?(meth="POST") ?(content_type="application/json") ?(content="{}")
+      msg url ?headers f =
+    make ~meth ?headers ~content_type ~content url msg f
 end
 
 include EzRequest.Make(Interface)
--- libs/request/unix/curl/ezCurl_lwt.ml
+++ libs/request/unix/curl/ezCurl_lwt.ml
@@ -1,58 +1,24 @@
-let verbose = match Sys.getenv_opt "EZAPICLIENT" with
-  | Some "true" | Some "1" -> true
-  | _ -> false
-
-let log ?(meth="GET") url = function
-  | None -> ()
-  | Some msg -> Printf.printf "[>%s %s %s ]\n%!" msg meth url
-
-let writer_callback a d =
-  Buffer.add_string a d;
-  String.length d
-
-let initialize_connection url =
-  let r = Buffer.create 16384
-  and c = Curl.init () in
-  Curl.set_timeout c 30;      (* Timeout *)
-  Curl.set_sslverifypeer c false;
-  Curl.set_sslverifyhost c Curl.SSLVERIFYHOST_EXISTENCE;
-  Curl.set_writefunction c (writer_callback r);
-  Curl.set_tcpnodelay c true;
-  Curl.set_verbose c verbose;
-  Curl.set_post c false;
-  Curl.set_url c url;
-  r,c
-
-let make ?msg ~headers prepare url =
+let make ?msg ?meth ?content ?content_type ?headers url =
+  EzCurl_common.log ?meth url msg;
   let r () =
-    let r, c = initialize_connection url in
-    prepare c;
-    Curl.set_httpheader c (
-      List.map (fun (name, value) -> Printf.sprintf "%s: %s" name value) headers);
+    let r, c = EzCurl_common.init ?meth ?content ?content_type ?headers url in
     Lwt.map (fun _code ->
         let rc = Curl.get_responsecode c in
         Curl.cleanup c;
         let data = Buffer.contents r in
-        log ~meth:("RECV " ^ string_of_int rc) url msg;
+        EzCurl_common.log ~meth:("RECV " ^ string_of_int rc) url msg;
         if rc >= 200 && rc < 300 then Ok data
         else Error (rc, Some data))
       (Curl_lwt.perform c) in
   Lwt.catch r (fun exn -> Lwt.return (Error (-1, Some (Printexc.to_string exn))))
 
 module Interface = struct
-  let get ?(meth="GET") ?(headers=[]) ?msg url =
-    log ~meth url msg;
-    make ?msg ~headers (fun c -> Curl.set_post c false) url
+  let get ?(meth="GET") ?headers ?msg url =
+    make ?msg ~meth ?headers url
 
-  let post ?(meth="POST") ?(content_type = "application/json") ?(content="{}") ?(headers=[])
+  let post ?(meth="POST") ?(content_type="application/json") ?(content="{}") ?headers
       ?msg url =
-    log ~meth url msg;
-    let headers = ("Content-Type", content_type) :: headers in
-    make ?msg ~headers (fun c ->
-        if meth = "PUT" then Curl.set_put c true else Curl.set_post c true;
-        Curl.set_postfields c content;
-        Curl.set_postfieldsize c (String.length content);
-      ) url
+    make ?msg ~meth ?headers ~content ~content_type url
 end
 
 include EzRequest_lwt.Make(Interface)
