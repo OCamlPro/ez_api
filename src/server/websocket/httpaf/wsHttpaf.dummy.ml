a1f16ec271654a0333cd533a2d3ffab93a277e7e
Maxime Levillain <maxime.levillain@origin-labs.com>
2021-05-01 16:26:17 +0200

    dummy geoip to remove optional flags of ez_api.server et ez_api.server_utils

--- src/server/websocket/httpaf/wsHttpaf.dummy.ml
+++ src/server/websocket/httpaf/wsHttpaf.dummy.ml
@@ -1,2 +1,11 @@
+let first = ref true
+
 let ws _fd _req ?onclose:_ ?step:_ ~react:_ ~bg:_ _id =
+  (if !first then (
+      first := false;
+      Format.eprintf
+        "\027[0;31merror: websocket-httpaf-lwt not installed\027[0m\n\
+         try: `opam install calendar`\n\
+         `opam pin add websocket-httpaf.~dev https://github.com/anmonteiro/websocket-httpaf.git`\n\
+         `opam pin add websocket-httpaf-lwt.~dev https://github.com/anmonteiro/websocket-httpaf.git`@."));
   Lwt.return_error `no_ws_library
