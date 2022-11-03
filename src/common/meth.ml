7ab642b1abdf2afd80cd28170e00cd45d78804ea
Max <maxime.levillain@functori.com>
2022-11-03 11:16:05 +0100

    put methods name in capital (problem with some browsers)

--- src/common/meth.ml
+++ src/common/meth.ml
@@ -3,13 +3,13 @@ type t = [ `GET | `POST | `PUT | `DELETE | `PATCH ]
 type all = [ t | empty ]
 
 let to_string : [< all ] -> string = function
-  | `GET -> "get"
-  | `POST -> "post"
-  | `PUT -> "put"
-  | `DELETE -> "delete"
-  | `PATCH -> "patch"
-  | `OPTIONS -> "options"
-  | `HEAD -> "head"
+  | `GET -> "GET"
+  | `POST -> "POST"
+  | `PUT -> "PUT"
+  | `DELETE -> "DELETE"
+  | `PATCH -> "PATCH"
+  | `OPTIONS -> "OPTIONS"
+  | `HEAD -> "HEAD"
 
 let headers l =
   let meths = String.concat "," @@ List.map to_string l in
